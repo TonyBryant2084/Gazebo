16bca5781152368e6ed11c7287c7b4830c3ae95e
TonyBryant2084 <tonybryant2084@gmail.com>
2026-09-17 23:24:45 +0800

    Gazebo

---
+++ src/mycobot_ros2/mycobot_description/urdf/robots/test.sh
@@ -0,0 +1,12 @@
+xacro mycobot_280.urdf.xacro \
+robot_name:=mycobot_280 \
+prefix:= \
+add_world:=true \
+base_link:=base_link \
+base_type:=g_shape \
+flange_link:=link6_flange \
+gripper_type:=adaptive_gripper \
+use_camera:=false \
+use_gazebo:=true \
+use_gripper:=true \
+> mycobot_full_export.urdf
